4ff0a6f2f551c982cb029522ae3d5f94f55abf85
kipriox <kipriox@mail.ru>
2022-03-23 21:03:54 +0300

    Тренировка и прогнозирование

--- DiamondsPredicting.py
+++ DiamondsPredicting.py
@@ -22,4 +22,23 @@ for i in range(3):
 	new = le.fit_transform(df[categorical_features[i]])
 	df[categorical_features[i]] = new
 	
-print(df.head(10).to_string())
+# ~ print(df.head(10).to_string())
+
+x = df[['carat', 'cut', 'color','clarity', 'depth','table', 'x', 'y', 'z']]
+y = df[['price']]
+
+# Разделение данных на тренировочный и тестовый наборы
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 25, random_state = 101)
+
+# Тренировка
+regr = RandomForestRegressor(n_estimators = 10, max_depth = 10, random_state = 101)
+regr.fit(x_train, y_train.values.ravel())
+
+#Прогнозирование
+predictions = regr.predict(x_test)
+
+result = x_test
+result['price'] = y_test
+result['prediction'] = predictions.tolist()
+
+print(result.to_string())
