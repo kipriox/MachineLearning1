5bbed0b277b36aac10371e4276706656a5845316
kipriox <kipriox@mail.ru>
2022-03-23 20:39:46 +0300

    Изменение файла P1_diamonds.csv

--- DiamondsPredicting.py
+++ DiamondsPredicting.py
@@ -8,4 +8,18 @@ from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('P1_diamonds.csv')
 
+# ~ print(df.head(10).to_string())
+
+# Удаление Unnamed сещлбца, axis = 1 означает, что мы удаляем столбец
+df = df.drop(['Unnamed: 0'], axis = 1)
+
+# Создание переменных для категорий
+categorical_features = ['cut', 'color', 'clarity']
+le = LabelEncoder()
+
+# Замена категорий на численные значения
+for i in range(3):
+	new = le.fit_transform(df[categorical_features[i]])
+	df[categorical_features[i]] = new
+	
 print(df.head(10).to_string())
